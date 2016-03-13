4a4ec1cf3ee279da813ce64060ad5527ae673a5e
Cheyenne Pillay <Cheyenneivan@gmail.com>
2016-03-13 12:56:01 +0200

    default method and reset method that both set up matrix for a new game

--- Tut4-TicTacToe/TicTacToe.cpp
+++ Tut4-TicTacToe/TicTacToe.cpp
@@ -3,12 +3,22 @@
 using namespace std;
 
 
-// default constructor
+// default constructor that sets up matrix for a new game 
 TicTacToe::TicTacToe()
 {
+	for (int i = 1; i <= 3; i++)
+		for (int j = 1; j <= 3; j++)
+			mat[i][j] = '*';
 }
 
 
 TicTacToe::~TicTacToe()
 {
 }
+
+void TicTacToe::reset()
+{
+	for (int i = 1; i <= 3; i++)
+		for (int j = 1; j <= 3; j++)
+			mat[i][j] = '*';
+};
