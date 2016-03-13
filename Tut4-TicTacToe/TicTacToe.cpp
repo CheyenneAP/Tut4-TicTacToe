6e126bf90803d45b86a91625adc0f143156cb2cc
Cheyenne Pillay <Cheyenneivan@gmail.com>
2016-03-13 13:01:48 +0200

    print method to print the current board  showing all plays that have been made

--- Tut4-TicTacToe/TicTacToe.cpp
+++ Tut4-TicTacToe/TicTacToe.cpp
@@ -16,9 +16,23 @@ TicTacToe::~TicTacToe()
 {
 }
 
-void TicTacToe::reset()
-{
+// clears the board to start a new game 
+void TicTacToe::reset(){
+
 	for (int i = 1; i <= 3; i++)
 		for (int j = 1; j <= 3; j++)
 			mat[i][j] = '*';
 };
+
+// method to print the current board showing all plays that have been made
+void TicTacToe::print(){
+	for (int i = 1; i <= 3; i++){
+
+		for (int j = 1; j <= 3; j++)
+			// display the contents of the matrix
+			cout << mat[i][j] << "\t";
+		cout << endl;
+	};
+};
+
+
